da83ccefb548a9e6f77dfa71bc595249ea249baf
Brett Smith <brettsmith212@gmail.com>
2022-03-01 09:06:24 -0800

    successfully adding potlucks

--- api/potluck/potluck-model.js
+++ api/potluck/potluck-model.js
@@ -1,1 +1,25 @@
 const db = require("../data/db-config");
+
+async function allPotlucks() {
+  return await db("potluck");
+}
+
+async function findPotluck(potluck_id) {
+  return await db("potluck").where("potluck_id", potluck_id).first();
+}
+
+async function addPotluck(potluck) {
+  let [newPotluck] = await db("potluck").insert(potluck, [
+    "potluck_id",
+    "date",
+    "time",
+    "location",
+  ]);
+  return newPotluck;
+}
+
+module.exports = {
+  allPotlucks,
+  findPotluck,
+  addPotluck,
+};
--- api/potluck/potluck-router.js
+++ api/potluck/potluck-router.js
@@ -1,7 +1,26 @@
 const router = require("express").Router();
+const { addPotluck, allPotlucks } = require("./potluck-model");
 
 router.get("/", (req, res) => {
-  res.json({ message: "getting potluck info" });
+  allPotlucks()
+    .then((potlucks) => {
+      res.status(200).json(potlucks);
+    })
+    .catch(() => {
+      res.status(500).json({ message: "error getting potlucks" });
+    });
+});
+
+router.post("/", (req, res) => {
+  const potluck = req.body;
+
+  addPotluck(potluck)
+    .then((newPotluck) => {
+      res.status(201).json(newPotluck);
+    })
+    .catch(() => {
+      res.status(500).json({ message: "error adding potluck" });
+    });
 });
 
 module.exports = router;
